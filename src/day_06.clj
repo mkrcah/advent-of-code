b42ea48d8c0007dabe0f994950375299b03c28e8
Marcel Krcah <143517863+mkrcah-vandebron@users.noreply.github.com>
2023-12-06 12:30:44 +0100

    2023, day 6, refactor

--- src/day_06.clj
+++ src/day_06.clj
@@ -3,50 +3,54 @@
   (:require [clojure.string :as str]
             [clojure.math :as m]))
 
-(defn read-file [f]
-  (->> f slurp str/split-lines))
-
-(defn parse-longs [ln]
-  (->> ln (re-seq #"\d+") (mapv parse-long)))
-
-(defn parse-input [input]
-  (->> input (mapv parse-longs) (apply map vector)))
-
 (defn roots
-  "Find root of ax^2 + bx + c = 0"
+  "Find root of ax^2 + bx + c = 0, assuming a!=0"
   [a b c]
   (let [disc (m/sqrt (- (* b b) (* 4 a c)))]
     [(/ (+ (- b) disc) (* 2 a))
      (/ (- (- b) disc) (* 2 a))]))
 
-(defn next-long [x] (long (if (= x (m/floor x)) (inc x) (m/ceil x))))
-(defn prev-long [x] (long (if (= x (m/ceil x)) (dec x) (m/floor x))))
+(defn whole? [x] (= x (m/floor x)))
+(defn next-long [x] (long (if (whole? x) (inc x) (m/ceil x))))
+(defn prev-long [x] (long (if (whole? x) (dec x) (m/floor x))))
 
-(defn winning-times [[t max-d]]
-  (let [[r1 r2] (roots (- 1) t (- max-d))
-        _ (println r1 r2)]
+(defn winning-times
+  "race_time = t - t_chosen
+   velocity = t_chosen
+   distance = race_time * velocity
+            = (t - t_chosen) * t_chosen
+  From there, we want to find t_chosen such that:
+  - t_chosen^2 + t * t_chosen - distance > 0"
+  [[t max-d]]
+  (let [[r1 r2] (roots (- 1) t (- max-d))]
     [(next-long r1) (prev-long r2)]))
 
-(defn ways-to-win [pairs]
-  (->> pairs
-     (map winning-times)
-     (map (fn [[min max]] (inc (- max min))))
-     (reduce *)))
+(defn ways-to-win [[min max]] (inc (- max min)))
 
-; part 2
-(defn parse-input-kern [input]
-  [(->> input
-        (map (fn [ln] (str/replace ln #"[^0-9]" "")))
-        (map parse-long))])
+(def read-file (comp str/split-lines slurp))
+(def transpose (partial apply map vector))
 
-(-> "input/day-06.txt" read-file parse-input ways-to-win)
-(-> "input/day-06.txt" read-file parse-input-kern ways-to-win)
+(defn solve1 [lns]
+  (let [parse-lns (comp (map (partial re-seq #"\d+"))
+                        (map (partial map parse-long)))]
+    (->> lns
+         (sequence parse-lns)
+         transpose
+         (map winning-times)
+         (map ways-to-win)
+         (reduce *))))
+
+(defn solve2 [lns]
+  (let [parse-lns (comp (map (fn [ln] (str/replace ln #"[^0-9]" "")))
+                        (map parse-long))]
+    (->> lns
+         (sequence parse-lns)
+         winning-times
+         ways-to-win)))
+
+(solve1 (read-file "input/day-06.txt"))
+(solve2 (read-file "input/day-06.txt"))
 
 (comment
-  (def example (read-file "input/example-06.txt"))
-  (ways-to-win (parse-input example)) ; should be 288
-  (ways-to-win (parse-input-kern example))) ; should be 71503
-
-
-
-
+  (solve1 (read-file "input/example-06.txt")) ; 288
+  (solve2 (read-file "input/example-06.txt"))) ; 71503
